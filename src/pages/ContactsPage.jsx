5926af800899f792135ba8c8d3fe302031e56e3e
ikspass <moolingtoon@mail.ru>
2025-05-30 21:48:51 +0300

    done contacts page

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -11,24 +11,30 @@ const ContactsPage = observer(() => {
 
   const user = userStore.user
   console.log(user)
+  const instructor = user.student.instructor;
+  const teacher = user.student.group.teacher;
 
-  const data = user.student 
+  const data = user.student //instructorID и groupID != null добавить 
   ? 
-  [user.student.instructor, user.student.group.teacher] 
+  [
+    {id: instructor.id, role: 'Инструктор', fullName: instructor.user.fullName, phoneNumber: instructor.user.phoneNumber, status: instructor.status },
+    {id: teacher.id, role: 'Преподаватель', fullName: teacher.user.fullName, phoneNumber: teacher.user.phoneNumber, status: teacher.status},
+  ]
   : 
   [
-    { role: { description: 'Инструктор' } },
-    { role: { description: 'Преподаватель' } }
+    { role: 'Преподаватель' },
+    { role: 'Инструктор' },
   ];
 
   console.log('data: ', data)
 
   return (
-    <div>
+    <div className='filter-container'>
+      <p className="heading-text-2">Контакты</p>
       <InformationTable 
         columns={[
-          { key: "role.description", label: "Должность", isLink: false },
-          { key: "fullName", label: "ФИО", isLink: true, navigateTo: (row) => row.role.value == 'teacher' ? `${TEACHER_ROUTE}/${row.teacher.id}` : `${INSTRUCTOR_ROUTE}/${row.instructor.id}`},
+          { key: "role", label: "Должность", isLink: false },
+          { key: "fullName", label: "ФИО", isLink: true, navigateTo: (row) => row.role == 'Преподаватель' ? `${TEACHER_ROUTE}/${row.id}` : `${INSTRUCTOR_ROUTE}/${row.id}`},
           { key: "phoneNumber", label: "Номер телефона", isLink: false },
           { key: "status", label: "Статус", isLink: false },
         ]}
